06bd571d358c058c8550c166298bd4835dc80bea
josesangeeth14 <jose13y14@gmail.com>
2019-08-26 12:00:37 +0530

    Excel integration code

--- src/main/java/org/snapdeal/excel/test/SnapdealExcel/Excel.java
+++ src/main/java/org/snapdeal/excel/test/SnapdealExcel/Excel.java
@@ -2,7 +2,6 @@ package org.snapdeal.excel.test.SnapdealExcel;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,37 +13,43 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Excel {
-	public static String getData(int rowno,int columnno) throws Throwable
+	public static String getData(int rowno,int columnno) 
 	 {
-		File loc = new File("G:\\Workspace\\SnapdealExcel\\Excelfile\\Book2.xlsx");
-		  FileInputStream f = new FileInputStream(loc);
-		  Workbook w = new XSSFWorkbook(f);
-		  Sheet s = w.getSheet("Josee");
-		  Row r = s.getRow(rowno);
-		  Cell c = r.getCell(columnno);
-		  int type = c.getCellType();
-		  String txtvalue ="";
-		  
-		  if(type==1)
-		  {
-			  txtvalue = c.getStringCellValue();
-		  }
-		  else if(type==0)
-		  {
-			  double d = c.getNumericCellValue();
-			  long l = (long)d;
-			  txtvalue = String.valueOf(l);
-		  }
-		  else
-		  {
-			  if(DateUtil.isCellDateFormatted(c))
+		 String txtvalue ="";
+		try {
+			File loc = new File("G:\\Workspace\\SnapdealExcel\\Excelfile\\Book2.xlsx");
+			  FileInputStream f = new FileInputStream(loc);
+			  Workbook w = new XSSFWorkbook(f);
+			  Sheet s = w.getSheet("Josee");
+			  Row r = s.getRow(rowno);
+			  Cell c = r.getCell(columnno);
+			  int type = c.getCellType();
+			 
+			  
+			  if(type==1)
 			  {
-				  Date d = c.getDateCellValue();
-				  SimpleDateFormat frmt = new SimpleDateFormat("dd-MMM-yy");
-				  txtvalue = frmt.format(d);
+				  txtvalue = c.getStringCellValue();
 			  }
-		  }
-		 
+			  else if(type==0)
+			  {
+				  double d = c.getNumericCellValue();
+				  long l = (long)d;
+				  txtvalue = String.valueOf(l);
+			  }
+			  else
+			  {
+				  if(DateUtil.isCellDateFormatted(c))
+				  {
+					  Date d = c.getDateCellValue();
+					  SimpleDateFormat frmt = new SimpleDateFormat("dd-MMM-yy");
+					  txtvalue = frmt.format(d);
+				  }
+			  }
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+				 
 		return txtvalue;
 	 }
 public static void main(String[] args) throws Throwable {
